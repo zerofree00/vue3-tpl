b0392445d947a4560e04d52a1af218382fc6c20a
zerofree00 <2847426033@qq.com>
2021-09-01 18:30:22 +0800

    fixed

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,16 +1,85 @@
 /**
- *  @author
+ * @author
  * @description  按需引入Vant
  */
 
 // 引入全局样式
 import '@/assets/scss/index.scss'
-import { Button, Tabbar, TabbarItem } from 'vant'
-const pluginsVant = [Button, Tabbar, TabbarItem]
+
+import {
+  Button,
+  Tabbar,
+  TabbarItem,
+  ActionBar,
+  ActionBarIcon,
+  ActionBarButton,
+  Divider,
+  Popup,
+  Overlay,
+  Loading,
+  Dialog,
+  ContactCard,
+  Form,
+  AddressEdit,
+  AddressList,
+  Field,
+  CellGroup,
+  Cell,
+  SwipeCell,
+  Icon,
+  Stepper,
+  Card,
+  Checkbox,
+  CheckboxGroup,
+  Swipe,
+  SwipeItem,
+  PullRefresh,
+  List,
+  Tab,
+  Tabs,
+  SubmitBar,
+  Toast
+} from 'vant'
+
+const pluginsVant = [
+  Button,
+  Tabbar,
+  TabbarItem,
+  ActionBar,
+  ActionBarIcon,
+  ActionBarButton,
+  Divider,
+  Popup,
+  Overlay,
+  Loading,
+  Dialog,
+  ContactCard,
+  Form,
+  AddressEdit,
+  AddressList,
+  Field,
+  CellGroup,
+  Cell,
+  SwipeCell,
+  Icon,
+  Stepper,
+  Card,
+  Checkbox,
+  CheckboxGroup,
+  Swipe,
+  SwipeItem,
+  PullRefresh,
+  List,
+  Tab,
+  Tabs,
+  SubmitBar,
+  Toast
+]
+
 export const vantPlugins = {
   install: function (vm) {
     pluginsVant.forEach(item => {
-      vm.component(item.name, item)
+      vm.use(item)
     })
   }
 }
